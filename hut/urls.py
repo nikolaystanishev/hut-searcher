108c8f3d57eed883d64946a9fffca59e0851f64d
nikolaystanishev <stanishev.nikolay@gmail.com>
2017-04-27 17:46:35 +0300

    Update urls

--- hut/urls.py
+++ hut/urls.py
@@ -3,14 +3,15 @@ from django.conf.urls import url
 from hut import views
 
 urlpatterns = [
-    url(r'huts', views.all_huts),
-    url(r'add_hut', views.add_hut),
-    url(r'hut/(?P<hutname>[A-Za-z0-9]+)', views.show_hut),
-    url(r'update_hut/(?P<hutname>[A-Za-z0-9]+)', views.update_hut),
-    url(r'huts/(?P<mountain>[A-Za-z]+)', views.huts_by_mountain),
-    url(r'huts/(?P<mountain>[A-Za-z0-9]+)/(?P<hutname>[A-Za-z0-9]+)',
+    url(r'^huts$', views.all_huts),
+    url(r'^add_hut$', views.add_hut),
+    url(r'^hut/(?P<hutname>[A-Za-z0-9]+)$', views.show_hut),
+    url(r'^update_hut/(?P<hutname>[A-Za-z0-9]+)$', views.update_hut),
+    url(r'^huts/(?P<mountain>[A-Za-z]+)$', views.huts_by_mountain),
+    url(r'^huts/(?P<mountain>[A-Za-z0-9]+)/(?P<hutname>[A-Za-z0-9]+)$',
         views.hut_by_name),
-    url(r'huts/(?P<mountain>[A-Za-z0-9]+)/?page=n&size=l', views.page_of_huts),
-    # url(r'/hut/(?P<mountain>[A-Za-z0-9]+)?query', views.huts_by_parameters),
+    url(r'^huts/(?P<mountain>[A-Za-z0-9]+)/?page=n&size=l$',
+        views.page_of_huts),
+    # url(r'/hut/(?P<mountain>[A-Za-z0-9]+)?query$', views.huts_by_parameters),
     url(r'^$', views.home),
 ]
